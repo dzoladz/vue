3a7a4e9d6c1f522737f1e05f3467a7abc5041862
Derek Zoladz <derek@derekzoladz.com>
2023-06-12 21:46:22 -0400

    ADD: axios

---
+++ src/views/AxiosView.vue
@@ -0,0 +1,21 @@
+<script>
+import axios from "axios";
+export default {
+  data: () => ({
+    results: null
+  }),
+  created() {
+    axios.get("https://gorest.co.in/public/v2/users").then((result) => {
+      this.results = result.data;
+    })
+  }
+};
+</script>
+
+<template>
+  <div class="content">
+    <div v-for="result in results">
+      {{ result.id }} | {{ result.name}} | {{ result.email }}<br>
+    </div>
+  </div>
+</template>
